73d4da1c1592be24a9e09216adcdc1c789b34765
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-05 15:42:04 +0100

    feat: ajout des routes pour la création de compte

--- flashCards/start/routes.ts
+++ flashCards/start/routes.ts
@@ -18,8 +18,14 @@ import { middleware } from './kernel.js'
 router.on('/').render('pages/home').use(middleware.auth())
 
 //[Authentification]
-router.get('/login', [AuthController, 'index']).as('auth.show').use(middleware.guest()) //Show form
-router.post('/login', [AuthController, 'login']).as('auth.login').use(middleware.guest()) //Treat from
+router.get('/login', [AuthController, 'logInIndex']).as('auth.show').use(middleware.guest()) //Show form login
+router.post('/login', [AuthController, 'login']).as('auth.login').use(middleware.guest()) //Treat from login
+
+router
+  .get('/register', [AuthController, 'registerIndex'])
+  .as('auth.register.show')
+  .use(middleware.guest())
+router.post('/register', [AuthController, 'register']).as('auth.register')
 
 router.get('/logout', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
 
