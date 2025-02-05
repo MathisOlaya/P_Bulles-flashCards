80f641b0d239ee4002d4471d6b24ef326c58404d
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-05 14:34:15 +0100

    fix: changer la taille des champs (le password hashé était trop long)

--- flashCards/database/migrations/1738163743720_create_t_users_table.ts
+++ flashCards/database/migrations/1738163743720_create_t_users_table.ts
@@ -6,8 +6,9 @@ export default class TUserSchema extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id_user').notNullable() // Clé primaire
-      table.string('username', 80).notNullable().unique() // Nom unique
-      table.string('password_hash', 80).notNullable() // Hash du mot de passe
+      table.string('username', 255).notNullable().unique() // Nom unique
+      table.boolean('is_admin').notNullable().defaultTo(false)
+      table.string('password_hash', 255).notNullable() // Hash du mot de passe
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
