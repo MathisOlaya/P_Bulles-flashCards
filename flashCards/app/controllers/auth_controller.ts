8697518b6654b16a18d477c2062f3f24c490c442
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-05 14:31:09 +0100

    chore: create auth controller

---
+++ flashCards/app/controllers/auth_controller.ts
@@ -0,0 +1,10 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+//Models
+import User from '#models/user'
+
+//Import Validator
+import { loginUserValidator } from '#validators/auth'
+
+export default class AuthController {
+}
