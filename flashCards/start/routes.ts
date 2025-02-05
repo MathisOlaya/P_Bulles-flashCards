ef6cb99821acdb449df6f4775d2f13379c149b02
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-05 14:35:53 +0100

    feat: définition des routes et vérifier que l'utilisateur est authentifié pour accéder à certaines pages

--- flashCards/start/routes.ts
+++ flashCards/start/routes.ts
@@ -6,11 +6,18 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
+//MiddleWare
 import router from '@adonisjs/core/services/router'
 
 //controller
 import DecksController from '#controllers/decks_controller'
+import AuthController from '#controllers/auth_controller'
+import { middleware } from './kernel.js'
 
 //routes
-router.get('/', [DecksController, 'index']).as('home')
+//[Authentification]
+router.get('/', [AuthController, 'index']).as('auth.show')
+router.post('/login', [AuthController, 'login']).as('auth.login')
+
+//[Home page]
+router.get('/home', [DecksController, 'index']).as('home').use(middleware.auth())
