36fda1531bccc9d5ca5bb386e763fc8dec72fccc
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-12 16:28:55 +0100

    feat: ajout de la route pour le deck

--- flashCards/start/routes.ts
+++ flashCards/start/routes.ts
@@ -14,20 +14,34 @@ import DecksController from '#controllers/decks_controller'
 import AuthController from '#controllers/auth_controller'
 import { middleware } from './kernel.js'
 
-//routes
-router.on('/').render('pages/home').use(middleware.auth())
+// [Routes] : Default
+router.on('/').redirect('/home').use(middleware.auth())
 
-//[Authentification]
-router.get('/login', [AuthController, 'logInIndex']).as('auth.login.show').use(middleware.guest()) //Show form login
+// [Authentification] : Login
+router.on('/login').render('pages/auth/login').as('auth.login.show').use(middleware.guest()) //Show form login
 router.post('/login', [AuthController, 'login']).as('auth.login').use(middleware.guest()) //Treat from login
 
+// [Authentification] : Register
 router
-  .get('/register', [AuthController, 'registerIndex'])
+  .on('/register')
+  .render('pages/auth/register')
   .as('auth.register.show')
   .use(middleware.guest())
 router.post('/register', [AuthController, 'register']).as('auth.register')
 
+// [Authentification] : Logout
 router.get('/logout', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
 
-//[Home page]
+// [Home page]
 router.get('/home', [DecksController, 'index']).as('home').use(middleware.auth())
+
+// [Deck]
+router
+  .on('/deck/create')
+  .render('pages/deck/createDeck')
+  .as('deck.create.show')
+  .use(middleware.auth())
+router.post('/deck/create', [DecksController, 'create']).as('deck.create').use(middleware.auth())
+
+// Show deck's page
+router.get('/deck/:id/show', [DecksController, 'show']).as('deck.show').use(middleware.auth())
