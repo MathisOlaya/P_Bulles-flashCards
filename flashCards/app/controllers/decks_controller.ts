f74cdd7cb6053838c53267c9e6babed9f8d57f89
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-12 16:29:56 +0100

    feat: créer le deck

--- flashCards/app/controllers/decks_controller.ts
+++ flashCards/app/controllers/decks_controller.ts
@@ -1,14 +1,37 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
-//models
+// [Models]
 import Deck from '#models/deck'
+
+// [Validator]
+import { createDeckValidator } from '#validators/deck'
 
 export default class DecksController {
   async index({ view, auth }: HttpContext) {
+    //get user
+    const user = await auth.getUserOrFail()
+
     //get how many decks he has
-    const decks = await Deck.query()
+    const decks = await Deck.query().where('id_user', user.id_user)
     const deckCount = decks.length
 
-    return view.render('pages/home', { deckCount })
+    return view.render('pages/home', { decks, deckCount })
+  }
+  async show({ view }: HttpContext) {
+    return view.render('pages/deck/showDeck')
+  }
+  async create({ request, auth, response }: HttpContext) {
+    const { name, description } = await request.validateUsing(createDeckValidator)
+
+    //get user
+    const user = await auth.getUserOrFail()
+
+    await Deck.create({
+      nom: name,
+      description: description,
+      id_user: user.id_user,
+    })
+
+    return response.redirect().toRoute('home')
   }
 }
