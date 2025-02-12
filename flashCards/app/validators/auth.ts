6c61038afb4c5cabffa09370d9b38fdc5d0e0da8
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-12 14:08:14 +0100

    feat: vérifier que le nom d'utilisateur n'existe pas déjà

--- flashCards/app/validators/auth.ts
+++ flashCards/app/validators/auth.ts
@@ -3,8 +3,21 @@ import vine from '@vinejs/vine'
 const loginUserValidator = vine.compile(
   vine.object({
     username: vine.string(),
-    password: vine.string().minLength(4),
+    password: vine.string(),
   })
 )
 
-export { loginUserValidator }
+const registerUserValidator = vine.compile(
+  vine.object({
+    username: vine.string().unique(async (db, value) => {
+      const user = await db.from('t_user').where('username', value).first()
+      return !user
+    }),
+    password: vine.string().minLength(8).confirmed({
+      confirmationField: 'confirmPassword',
+    }),
+    confirmPassword: vine.string(),
+  })
+)
+
+export { loginUserValidator, registerUserValidator }
