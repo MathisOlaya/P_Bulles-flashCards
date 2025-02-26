a8054d7679d4cae81b3e3b43fb83017633462a8c
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-26 16:10:48 +0100

    feat(validator): on valide le nom du deck par rapport a son auteur. Voir description
    De cette manière, un utilisateur peut avoir uniquement un deck avec le meme nom. mais plusieurs utilisateurs peuvent avoir un deck avec le meme nom.

---
+++ flashCards/app/validators/card.ts
@@ -0,0 +1,39 @@
+import vine from '@vinejs/vine'
+
+const createCardValidator = (deckId) => {
+  return vine.compile(
+    vine.object({
+      question: vine
+        .string()
+        .trim()
+        .minLength(10)
+        .unique(async (db, value, field) => {
+          const user = await db
+            .from('t_card')
+            .where('question', value)
+            .andWhere('id_deck', deckId)
+            .first()
+          return !user
+        }),
+      answer: vine.string().trim().minLength(1),
+    })
+  )
+}
+
+const updateCardValidator = (deckId) =>
+  vine.compile(
+    vine.object({
+      question: vine.string().unique(async (db, value, field) => {
+        const user = await db
+          .from('t_card')
+          .where('question', value)
+          .andWhereNot('id_card', field.parent.params.cardId)
+          .andWhere('id_deck', deckId)
+          .first()
+        return !user
+      }),
+      reponse: vine.string().trim().minLength(1),
+    })
+  )
+
+export { createCardValidator, updateCardValidator }
--- flashCards/app/validators/deck.ts
+++ flashCards/app/validators/deck.ts
@@ -1,28 +1,33 @@
 import vine from '@vinejs/vine'
 
-const createDeckValidator = vine.compile(
-  vine.object({
-    name: vine.string().unique(async (db, value, field) => {
-      console.log(field)
-      const user = await db.from('t_deck').where('nom', value).first()
-      return !user
-    }),
-    description: vine.string().trim().minLength(10),
-  })
-)
+const createDeckValidator = (userId) => {
+  return vine.compile(
+    vine.object({
+      name: vine.string().unique(async (db, value, field) => {
+        console.log(field.meta)
+        const user = await db.from('t_deck').where('nom', value).andWhere('id_user', userId).first()
+        return !user
+      }),
+      description: vine.string().trim().minLength(10),
+    })
+  )
+}
 
-const updateDeckValidator = vine.compile(
-  vine.object({
-    name: vine.string().unique(async (db, value, field) => {
-      const user = await db
-        .from('t_deck')
-        .where('nom', value)
-        .andWhereNot('id_deck', field.parent.params.id)
-        .first()
-      return !user
-    }),
-    description: vine.string().trim().minLength(10),
-  })
-)
+const updateDeckValidator = (userId) => {
+  return vine.compile(
+    vine.object({
+      name: vine.string().unique(async (db, value, field) => {
+        const user = await db
+          .from('t_deck')
+          .where('nom', value)
+          .andWhereNot('id_deck', field.parent.params.id)
+          .andWhere('id_user', userId)
+          .first()
+        return !user
+      }),
+      description: vine.string().trim().minLength(10),
+    })
+  )
+}
 
 export { createDeckValidator, updateDeckValidator }
