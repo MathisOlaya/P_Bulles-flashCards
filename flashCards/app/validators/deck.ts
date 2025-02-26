a72e2bdfd701d00520a8faba7564cadabde149d3
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-02-26 12:38:04 +0100

    feat(validator): ne pas compter les espaces comme des caractères

--- flashCards/app/validators/deck.ts
+++ flashCards/app/validators/deck.ts
@@ -7,18 +7,21 @@ const createDeckValidator = vine.compile(
       const user = await db.from('t_deck').where('nom', value).first()
       return !user
     }),
-    description: vine.string().minLength(10),
+    description: vine.string().trim().minLength(10),
   })
 )
 
 const updateDeckValidator = vine.compile(
   vine.object({
     name: vine.string().unique(async (db, value, field) => {
-      console.log(field)
-      const user = await db.from('t_deck').where('nom', value).andWhereNot('id_deck', field.parent.params.id).first()
+      const user = await db
+        .from('t_deck')
+        .where('nom', value)
+        .andWhereNot('id_deck', field.parent.params.id)
+        .first()
       return !user
     }),
-    description: vine.string().minLength(10),
+    description: vine.string().trim().minLength(10),
   })
 )
 
