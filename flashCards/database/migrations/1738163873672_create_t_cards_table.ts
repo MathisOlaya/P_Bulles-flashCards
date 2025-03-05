47bbe341f532ad1f924a923364496db78aca27f3
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-03-05 16:26:59 +0100

    chore(db): mettre la taille de question et table a 254

--- flashCards/database/migrations/1738163873672_create_t_cards_table.ts
+++ flashCards/database/migrations/1738163873672_create_t_cards_table.ts
@@ -6,8 +6,8 @@ export default class TCardSchema extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id_card').notNullable() // Clé primaire
-      table.string('question', 70).notNullable() // Question unique
-      table.string('reponse', 70).notNullable() // Réponse
+      table.string('question', 254).notNullable() // Question unique
+      table.string('reponse', 254).notNullable() // Réponse
       table.integer('id_deck').unsigned().notNullable() // Clé étrangère vers t_deck
       table.foreign('id_deck').references('t_deck.id_deck').onDelete('CASCADE')
 
